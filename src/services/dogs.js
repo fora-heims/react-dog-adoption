b2c639dafb8eb3d14d8a020911852ae3532a8e9b
fora-heims <foraheims@gmail.com>
2022-01-05 11:31:19 -0800

    created functions for creating, updating, and deleting data from our supabase client.

--- src/services/dogs.js
+++ src/services/dogs.js
@@ -9,3 +9,17 @@ export async function getDog(id) {
   let request = await client.from('dogs').select('*').match({ id: id });
   return checkError(request);
 }
+
+export async function updateDog(dog) {
+  let request = await client.from('dogs').update(dog).eq('id', dog.id);
+  return checkError(request);
+}
+export async function addDog(dog) {
+  let request = await client.from('dogs').insert(dog);
+  return checkError(request);
+}
+
+export async function deleteDog(id) {
+  let request = await client.from('dogs').delete().match({ id });
+  return checkError(request);
+}
